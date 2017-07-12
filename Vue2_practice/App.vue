d10062db4ffdbf774eb400e3414a1270b7ea4dbc
isliangj <isliangj@icloud.com>
2017-07-12 22:30:46 +0800

    Webpack_Practice

---
+++ Vue2_practice/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/vue.png">
+    <h1> {{ msg }} </h1>
+    <firstcomponent></firstcomponent>
+  </div>
+</template>
+
+<script>
+import firstcomponent from './component/firstcomponent'
+export default {
+  data (){
+    return{
+      msg: 'Hello Vue!'
+    }
+  },
+  component:{
+    firstcomponent
+  }
+}
+</script>
+
+<style>
+body{
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ Vue2_practice/component/firstcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="firstcompoent">
+    <h1>I am a title</h1>
+    <a>written by {{ author}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      author:"isliangj"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
